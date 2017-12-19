c5096ef0ce1827d6bf44a6bc61117be147cb89a1
wangminjun0207 <wmjswjtu@163.com>
2017-12-19 22:35:10 +0800

    changed.

--- wangmingjun.cpp
+++ wangmingjun.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 int main(int argc, char *argv[])
 {
-	printf("%s,%d","pls take care of everthing!",2017)£»
+	printf("%s,%d","pls take care of everthing!",2017);
+	printf("ok!"); 
 	return 0; 
  } 
