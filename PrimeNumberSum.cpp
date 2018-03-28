2d4a5d51f48f74a3c58f88f122656675d475dda4
wangminjun0207 <wmjswjtu@163.com>
2018-03-28 21:38:20 +0800

    需要调试

---
+++ PrimeNumberSum.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <math.h>
+// 求 3~n （包括3和 n）所有素数的平方和 
+float func(int n)
+{
+	int i,j,flag;
+	float result = 0;
+	flag = 1;
+	for(i=3; i<=n; i++)
+	{
+		for(j=2; j<n; j++)
+		{
+			if(i%j == 0)
+			{
+				flag = 0;
+				break;
+			}	
+		}
+		if(flag == 1)
+			result = result + sqrt((float)i);
+		//printf("result = %f\n",sqrt(i));
+		flag = 1;
+	}
+	return result;
+}
+int main()
+{
+	int n;
+	printf("Input anumber :");
+	scanf("%d",&n);
+	printf("result = %f\n",func(n));
+	return 0;
+}
